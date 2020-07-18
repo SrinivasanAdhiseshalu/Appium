5b12492e762b6d9610ff75534abcd45efe4172be
srini <srini@srini-PC>
2020-07-18 11:30:23 +0530

    Renamed

--- AmazonApp/src/main/java/com/amazon/Utilities/PreRequesiteConfig.java
+++ AmazonApp/src/main/java/com/amazon/Utilities/PreRequesiteConfig.java
@@ -3,7 +3,7 @@ package com.amazon.Utilities;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-public class PreRequestConfig {
+public class PreRequesiteConfig {
 	
 	@BeforeSuite
 	void startProject(){
